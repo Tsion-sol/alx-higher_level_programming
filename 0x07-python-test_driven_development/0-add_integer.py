433ea0f65e24400766d275773ce37e4448b53737
Tsion-sol <stsion25@gmail.com>
2022-09-23 03:43:21 -0700

    test driven development

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
 """
-This is the "0-add_integer" module.
-The 0-add_integer module supplies one function, add_integer(a, b).
+This module defines `add_integer`
+
+The function returns the sum of a and b
 """
 
-def add_integer(a, b):
-    """Return the addition of two numbers."""
-    if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-        if type(b) is float:
-            b = int(b)
-            return a + b
+def add_integer(a, b=98):
+    """adds a and b
+
+    Args:
+    a (int): term 1
+    b (int, optional): term 2. Defaults to 98.
+
+    Raises:
+    TypeError: a and b must be integer
+
+    Returns:
+    int: sum of a and b
+    """
+
+    values = []
+    for x, param in [(a, 'a'), (b, 'b')]:
+        if isinstance(x, int):
+            values.append(x)
+        elif isinstance(x, float):
+            values.append(int(x))
+        else:
+            raise TypeError("{} must be an integer".format(param))
+
+        return sum(values)
--- 0x07-python-test_driven_development/6-max_integer_test.py
+++ 0x07-python-test_driven_development/6-max_integer_test.py
@@ -1,17 +1,42 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
+"""Unittests for max_integer
 """
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-    If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-            i += 1
-            return result
+import unittest
+import random
+
+
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+        """Define unit test for the max_integer function"""
+
+            def test_ordered_list(self):
+                        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+                            
+                                def test_max_in_the_middle(self):
+                                            self.assertEqual(max_integer([1,2,3,7,4,5,6]), 7)
+
+                                                def test_unordered_list(self):
+                                                            self.assertEqual(max_integer([4, 2, 3, 1]), 4)
+
+                                                                def test_empty_list(self):
+                                                                            self.assertEqual(max_integer([]), None)
+
+                                                                                def test_one_element_list(self):
+                                                                                            self.assertEqual(max_integer([5]), 5)
+
+                                                                                                def test_floats(self):
+                                                                                                            self.assertEqual(max_integer([1.5, 1.7, 2.8]), 2.8)
+
+                                                                                                                def test_string_implicit_ansii_values(self):
+                                                                                                                            self.assertEqual(max_integer('abcd'), 'd')
+
+                                                                                                                                def test_empty_string(self):
+                                                                                                                                            self.assertEqual(max_integer(''), None)
+
+
+                                                                                                                                            if __name__ == '__main__':
+                                                                                                                                                    unittest.main()
+
